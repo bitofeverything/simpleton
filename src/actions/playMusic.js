8a913816a17ff130b80404489b5ba2b840209386
bitprior <b.prieur@gmail.com>
2018-04-01 22:53:25 -0400

    messing around with playmusic/playlist

--- src/actions/playMusic.js
+++ src/actions/playMusic.js
@@ -10,15 +10,27 @@ module.exports = (bot, message, videoId) => {
 // https://stackoverflow.com/questions/47045805/playing-an-audio-file-using-discord-js-and-ytdl-core
     const voiceChannel = message.member.voiceChannel;
 
+    // console.log(message.guild.me.setDeaf(true, ))
+
+    bot.voiceConnections.map((snowflake, voiceConnection) => {
+      console.log(snowflake, voiceConnection)
+    })
+
     if(voiceChannel){
+
+      console.log(Object.keys(bot))
+      message.guild.me.setDeaf(false)
+
       voiceChannel.join()
     .then(connection => {
       if(videoId.length > 0){
 
         const stream = ytdl(url, {filter: 'audioonly'})
-        const dispatcher = connection.playStream(stream, )
+        const dispatcher = connection.playStream(stream, {volume: 0.2})
         dispatcher.on("end", end => {
-          voiceChannel.leave();
+console.log( "Audio done" )
+          message.guild.me.setDeaf(true, 'I have nothing to play')
+          // voiceChannel.leave();
         })
 
       }
@@ -28,17 +40,4 @@ module.exports = (bot, message, videoId) => {
     message.reply("If a song is requested, but no one is around to hear it, does it make a sound?")
   }
 
-    // const videoFetch = fetch('https://icanhazdadjoke.com/', {
-    //   'headers':{
-    //     'Accept' : 'application/json',
-    //     'Content-Type':'application/json'},
-    //   'method':'GET'
-    // })
-    // jokePromise.then(resp => {
-    //   return resp.json()
-    // }).then(jbody => {
-    //   console.log(jbody)
-    //   let person = message.author.username;
-    //   message.channel.send(`Alright ${person}, you hear this one? \n "${jbody.joke}"`)
-    // }).catch(e => {console.error(e)});
 }
--- src/actions/playlist.js
+++ src/actions/playlist.js
@@ -1,40 +1,8 @@
-import fetch from 'node-fetch';
-
-const playlist = () => {
-
-    logger.info("Maintaining Playlist");
-    props.playlist = {'tracks':[]}
-
-    const req = fetch('https://api.darksky.net/forecast/'+ auth.darkSky+ '/43.6630632,-79.3920922?units=ca');
-
-    let response = new Promise((resolve, reject) => {
-
-        req.then((resp) => {
-            if(resp.status !== 200){
-                reject(resp)
-                throw Error(response.statusText)
-            }
-
-            return resp.json();
-        }).then((json) => {
-            // regularWeatherNotice = darkskyWeather(json);
-            // weatherJsonObject = json;
-            logger.info("Returning Data "+ json.currently.summary);
-            resolve([json, json]);
-        }).catch((err) => { logger.error(err) })
-    });
-
-    return response
-}
 
 module.exports = (props) => {
-    weatherFetch().then((resp) => {
-      logger.info("Response received")
-      // logger.info(resp[0]);
-      // regularWeatherNotice = resp[0]
-      const weatherJsonObject = resp[1]
-
-      props.weather = weatherJsonObject;
-    }).catch(err => {logger.error(err)});
+  // This should be empty or otherwise pull from DB as needed.
+  if(!props.playlist){
+    props.playlist = []
+  }
 
 }
